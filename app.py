876d984ab329f3d833f0f6f0f6f4e775d637ff7b
Eren25 <l21590076@sjuanrio.tecnm.mx>
2025-12-09 22:44:31 -0600

    inicial commit

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 # Modelo Categoría
 class Category(db.Model):
     __tablename__ = 'categories'
@@ -50,10 +51,35 @@ def add_post():
         db.session.commit()
 
         return redirect(url_for('index'))
-    
-    #Aqui sigue si es GET
+        #Aqui sigue si es GET
     categories = Category.query.all()
     return render_template('create_post.html', categories=categories)
 
+@app.route('/post/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    categories = Category.query.all()
+
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.category_id = request.form.get('category_id')
+
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('edit_post.html', post=post, categories=categories)
+@app.route('/post/delete/<int:post_id>')
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
